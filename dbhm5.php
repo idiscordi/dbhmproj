23f89dd10ba37e22f91f8680736278746ae7c2c2
idiscordi <jtvnwa@gmail.com>
2017-04-18 17:02:31 -0500

    initial commit
    skeleton of script, no functionality in

---
+++ dbhm5.php
@@ -0,0 +1,93 @@
+<?php
+	$serveraddr = 'localhost';
+	$dbloginname = 'jtvaught'
+	$dbloginpw = "owoh4che";
+	$sqlreturn;
+	$retrow;
+?>
+<title>DB Homework 5 Project Site</title>
+<body>
+	<?php
+	
+		function dbConnect() {
+			
+		}
+		
+		function dbDisconnect() {
+			
+		}
+		
+		function dbAddStudent() {
+			
+		}
+		
+		function dbAddCourse() {
+			
+		}
+		
+		function dbAddApplication() {
+			
+		}
+		
+		function dbGetStudents() {
+			
+		}
+		
+		function dbGetCoursesByDept() {
+			
+		}
+		
+		function dbGetCoursesByStudent() {
+			
+		}
+		
+		function homepage() {
+			
+		}
+		
+		function addStudentPage() {
+			
+		}
+		
+		function addCoursePage() {
+			
+		}
+		
+		function addApplicationPage() {
+			
+		}
+		
+		function displayTablePage() {
+			
+		}
+		
+		function homePageButton () {
+			
+		}
+		
+		if (!isset($_REQUEST["pageChoice"]) {
+			#HTML FOR HOMEPAGE IF NO 
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "displayTable") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addStudent") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addCourse") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addApplication") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getStudents") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getCourseByDept") {
+			
+		}
+		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getCourseByStudent") {
+			
+		}
+	?>
+</body>
