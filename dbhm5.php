b0795f13d8a0dc556f1f453677bf594a0708c87f
idiscordi <jtvnwa@gmail.com>
2017-04-21 19:07:11 -0500

    Update dbhm5.php

--- dbhm5.php
+++ dbhm5.php
@@ -1,9 +1,10 @@
 <?php
 	$serveraddr = 'localhost';
-	$dbloginname = 'jtvaught'
+	$dbloginname = 'jtvaught';
 	$dbloginpw = "owoh4che";
 	$sqlreturn;
 	$retrow;
+	ini_set('display_errors', 1);
 ?>
 <title>DB Homework 5 Project Site</title>
 <body>
@@ -65,29 +66,44 @@
 			
 		}
 		
-		if (!isset($_REQUEST["pageChoice"]) {
-			#HTML FOR HOMEPAGE IF NO 
+		if (!isset($_REQUEST["pageChoice"]) || $_REQUEST["pageChoice"] == "default") {
+			#HTML FOR HOMEPAGE IF NO SELECTION SET
+			?>
+			<form method="POST">
+				<label for="Selection">Command: </label>
+				<select id="pageChoice" name="pageChoice">
+					<option value="default">Select Command</option>
+					<option value="addStudent">Add Student</option>
+					<option value="addCourse">Add Course</option>
+					<option value="addApplication">Add Enrollment App</option>
+					<option value="getStudents">Show All Students</option>
+					<option value="getCoursesByDept">Show Courses by Dept</option>
+					<option value="getCourseByStudent">Show Courses by Student</option>
+				</select>
+				<input type="submit" name="choice" value="choice"/>
+			</form>
+		<?php 
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "displayTable") {
-			
+			#PARSE AND DISPLAY TABLE
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addStudent") {
-			
+			#HTML FOR TAKING USER INPUT FOR ADDING A STUDENT
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addCourse") {
-			
+			#HTML FOR TAKING USER INPUT FOR ADDING A COURSE
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "addApplication") {
-			
+			#HTML FOR TAKING USER INPUT TO ADD AN APPLICATION
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getStudents") {
-			
+			#HANDLE LOGIC TO CALL AND GET STUDENTS
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getCourseByDept") {
-			
+			#HTML FOR TAKING USER INPUT TO GET COURSES BY DEPT
 		}
 		else if(isset($_REQUEST) && $_REQUEST["pageChoice"] == "getCourseByStudent") {
-			
+			#HTML FOR TAKING USER INPUT TO GET COURSES BY STUDENT
 		}
 	?>
 </body>
